c594a2c7889e16c10a5c6bd46de1b0a86b1e1d0f
Yura <bondprcuk@gmail.com>
2021-11-17 11:03:23 +0200

    presentation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,8 @@
 $('.promo__slider').slick({
     infinite: true,
     dots: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/promo/arrow_left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/promo/arrow_right.svg"></button>',
     responsive: [
         {
             breakpoint: 769,
@@ -18,4 +18,34 @@ $(document).ready(function() {
       $('.promo__burger,.promo__menu,.header__logo').toggleClass('active');
     });
   });
-  
+
+$('.presentation__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/presentation/arrow_left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/presentation/arrow_right.svg"></button>',
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          dots: true
+        }
+      },
+      {
+        breakpoint: 577,
+        settings: {
+          arrows: false,
+          dots: true
+        }
+      }
+    ]
+})
+
+$('.slider-dots .slider-dots__item').click(function() {
+  var $this = $(this);
+  $('.presentation__slider').slick('slickGoTo', $this.data('index'))
+});
+
+$('.slider-dots__item').click(function () {
+  $('.active').removeClass('active');
+  $(this).addClass("active")
+});
